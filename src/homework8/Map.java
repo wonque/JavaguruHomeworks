677fdbcd820163123a64b519729e0a06da25d4cb
Anatoliy Tolstih <a.tolstihs@gmail.com>
2018-09-08 16:29:40 +0300

    Formatted

--- src/homework8/Map.java
+++ src/homework8/Map.java
@@ -7,22 +7,18 @@ public class Map {
     public static void main(String[] args) {
         HashMap<String, Integer> someMap = new HashMap<>();
         Scanner scanner = new Scanner(System.in);
-        while(true) {
+        while (true) {
             System.out.println("Enter some input or press Enter: ");
             String ans = scanner.nextLine();
-            if (ans.equals("")){
+            if (ans.equals("")) {
                 break;
-            }else {
+            } else {
                 someMap.putIfAbsent(ans, 0);
-                someMap.computeIfPresent(ans, (key, value) -> value+1);
+                someMap.computeIfPresent(ans, (key, value) -> value + 1);
             }
 
         }
         System.out.println(someMap);
         System.out.println("Total values: " + someMap.values().size());
     }
-
-
-
-
 }
